367c93401c9d6babb4e49ecc605dde21a47ef077
kzrv04 <k.kobayashi0426@gmail.com>
2023-01-05 04:46:51 +0900

    [Add]ユーザ・会議室・予約追加APIおよびアプリケーションを作成

--- sql_app/main.py
+++ sql_app/main.py
@@ -10,7 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 def get_db():
-    db = SessionLocal
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -23,28 +23,28 @@ def get_db():
 ## read
 @app.get("/users", response_model=List[schemas.User])
 async def read_users(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=skip)
-    return {"users": users }
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 @app.get("/rooms", response_model=List[schemas.Room])
 async def read_rooms(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    users = crud.get_rooms(db, skip=skip, limit=skip)
-    return {"rooms": rooms }
+    rooms = crud.get_rooms(db, skip=skip, limit=limit)
+    return rooms
 
 @app.get("/bookings", response_model=List[schemas.Booking])
 async def read_bookings(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    users = crud.get_bookings(db, skip=skip, limit=skip)
-    return {"bookings": bookings }
+    users = crud.get_bookings(db, skip=skip, limit=limit)
+    return users
 
 ## create
 @app.post("/users", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 @app.post("/rooms", response_model=schemas.Room)
-async def create_room(room: schemas.Room, db: Session = Depends(get_db)):
+async def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db)):
     return crud.create_room(db=db, room=room)
 
 @app.post("/bookings", response_model=schemas.Booking)
-async def create_booking(booking: schemas.Booking, db: Session = Depends(get_db)):
+async def create_booking(booking: schemas.BookingCreate, db: Session = Depends(get_db)):
     return crud.create_booking(db=db, booking=booking)
